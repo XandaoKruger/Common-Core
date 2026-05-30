2bb5f8723ff6a5241331e0eacf7f2d6e65344e15
acano-kr <acano-kr@student.42.fr>
2026-05-30 13:48:40 +0100

    iniciando módulo 01

---
+++ Milestone2/Python_Modules/Python_Module_01/ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+def main() -> None:
+    name: str = "Tomato"
+    height: float = 1.2
+    age: int = 45
+    print(f"Name: {name}\nHeight: {height}\nAge: {age}")
+
+
+if __name__ == "__main__":
+    main()
