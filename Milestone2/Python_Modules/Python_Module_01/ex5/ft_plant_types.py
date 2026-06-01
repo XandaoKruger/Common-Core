6d940c804db503c867259e3837e6f08997436c60
Alexandre Cano Kruger <acano-kr@c1r5s3.42lisboa.com>
2026-06-01 14:34:14 +0100

    ex5

--- Milestone2/Python_Modules/Python_Module_01/ex5/ft_plant_types.py
+++ Milestone2/Python_Modules/Python_Module_01/ex5/ft_plant_types.py
@@ -53,9 +53,92 @@ class Plant():
 
 class Flower(Plant):
     """Classe especializada para flores"""
-    def __init__(
-        self, name, height, age, growth_rate, color: str, # has_bloomed: bool
-    ):
+    def __init__(self, name, height, age, growth_rate, color: str) -> None:
         super().__init__(name, height, age, growth_rate)
         self.color = color
-        # self.has_bloomed = has_bloomed    RESOLVER ISSO AQUI.
+        self.has_bloomed: bool = False
+
+    def show(self) -> None:
+        """Sobrepoe o show da classe mae para exbir os dois"""
+        super().show()
+        print(f"Color: {self.color}")
+        if not self.has_bloomed:
+            print(f"{self.name} has not bloomed yet")
+        else:
+            print(f"{self.name} is blooming beautifully!")
+
+    def bloom(self) -> None:
+        """Altera o estado da flor para floreida"""
+        self.has_bloomed = True
+
+
+class Tree(Plant):
+    """Classe espeializada em arvores"""
+    def __init__(
+        self, name, height, age, growth_rate, trunk_diameter: float
+    ) -> None:
+        super().__init__(name, height, age, growth_rate)
+        self.trunk_diameter = trunk_diameter
+
+    def show(self) -> None:
+        """Sobrepoe o show da clase mae para exibir o da arvore """
+        super().show()
+        print(f"Trunk diameter: {self.trunk_diameter}cm")
+
+    def produce_shade(self) -> None:
+        """Imprime a mensagem de sombra projetada"""
+        print(f"Tree {self.name} now produces a shade of ", end="")
+        print(f"{self.get_height()}cm long and {self.trunk_diameter}cm wide")
+
+
+class Vegetable(Plant):
+    """Classe especializada em vegetais"""
+    def __init__(
+        self, name, height, age, growth_rate, harvest_season: str
+    ) -> None:
+        super().__init__(name, height, age, growth_rate)
+        self.harvest_season = harvest_season
+        self.nutritional_value: int = 0
+
+    def show(self) -> None:
+        """Sobrepoe o show mae para este"""
+        super().show()
+        print(f"Harvest season: {self.harvest_season}")
+        print(f"nutritional value: {self.nutritional_value}")
+
+    def age_plant(self) -> None:
+        """Chama a mae e aumenta o valor nutricional"""
+        super().age_plant()
+        self.nutritional_value += 1
+
+    def grow(self) -> None:
+        """Chama o crescimento padrão da mãe"""
+        super().grow()
+
+
+if __name__ == "__main__":
+    print("=== Garden Plant Types ===")
+    print("=== Flower")
+
+    rose = Flower("Rose", 15.0, 10, 0.0, "red")
+    rose.show()
+    print("[asking the rose to bloom]")
+    rose.bloom()
+    rose.show()
+
+    print("=== Tree")
+
+    oak = Tree("Oak", 200.0, 365, 0.0, 5.0)
+    oak.show()
+    print("[asking the oak to produce shade]")
+    oak.produce_shade()
+
+    print("=== Vegetable")
+
+    tomato = Vegetable("Tomato", 5.0, 10, 2.1, "April")
+    tomato.show()
+    print("[make tomato grow and age for 20 days]")
+    for tomate in range(20):
+        tomato.age_plant()
+        tomato.grow()
+    tomato.show()
