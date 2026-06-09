8c7bbbcee035fd8bd00bcbf8a3c774304520e2e8
acano-kr <acano-kr@student.42.fr>
2026-06-09 15:01:22 +0100

    py mod 2 ex 0

---
+++ Milestone2/Python_Modules/Python_Module_02/ex0/ft_first_exception.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+def input_temperature(temp_str: str) -> int:
+    """Conversão para inteiro"""
+    return int(temp_str)
+
+def test_temperature():
+    """Input da temperatura"""
+    print("Input data is '25'")
+    try:
+        result = input_temperature("25")
+        print(f"Temperature is now {result}°C\n")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}\n")
+
+    print("Input data is 'abc'")
+    try:
+        result = input_temperature("abc")
+        print(f"Temperature is now {result}°C\n")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}\n")
+
+
+if __name__ == "__main__":
+    print("=== Garden Temperature ===\n")
+    test_temperature()
+    print("All tests completed - program didn't crash!")
