af7b0e3e1ffb7caf20c95ee8535006e9cad7a343
Alexandre Cano Kruger <acano-kr@c1r5s4.42lisboa.com>
2026-06-11 08:15:17 +0100

    correcao num print

--- Milestone2/Python_Modules/Python_Module_01/ex0/ft_garden_intro.py
+++ Milestone2/Python_Modules/Python_Module_01/ex0/ft_garden_intro.py
@@ -5,10 +5,10 @@ def main() -> None:
     name: str = "Rose"
     height: str = "25"
     age: str = "30"
-    print("=== Welcome to My Garden ===")
     print(f"Plant: {name}\nHeight: {height}cm\nAge: {age} days")
-    print("\n=== End of Program ===")
 
 
 if __name__ == "__main__":
+    print("=== Welcome to My Garden ===")
     main()
+    print("\n=== End of Program ===")
