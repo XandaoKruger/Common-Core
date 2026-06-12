66a4fd02623f67c7662d457f3d5a9ea7783dc129
Alexandre Cano Kruger <acano-kr@c1r5s4.42lisboa.com>
2026-06-12 12:47:00 +0100

    ex 3 mod 2

--- Milestone2/Python_Modules/Python_Module_02/ex3/ft_custom_errors.py
+++ Milestone2/Python_Modules/Python_Module_02/ex3/ft_custom_errors.py
@@ -1,3 +1,58 @@
 #!/usr/bin/env python3
 
-def 
+class GardenError(Exception):
+    pass
+
+
+class PlantError(GardenError):
+    """Mostrador de erros de plants"""
+    def __init__(self, message: str = "Unknown plant error") -> None:
+        super().__init__(message)
+
+
+def test_plant_error() -> None:
+    try:
+        raise PlantError("The tomato plant is wilting!")
+    except PlantError as e:
+        print(f"Caught PlantError: {e}")
+
+
+class WaterError(GardenError):
+    """Mostrador de erros de water"""
+    def __init__(self, message: str = "Unknown water error") -> None:
+        super().__init__(message)
+
+
+def test_water_error() -> None:
+    try:
+        raise WaterError("Not enough water in the tank!")
+    except WaterError as e:
+        print(f"Caught WaterError: {e}")
+
+
+def all_garden_errors_test() -> None:
+    """Testador de todos os erros"""
+    try:
+        raise PlantError("The tomato plant is wilting!")
+    except GardenError as e:
+        print(f"Caught GardenError: {e}")
+
+    try:
+        raise WaterError("Not enough water in the tank!")
+    except GardenError as e:
+        print(f"Caught GardenError: {e}")
+
+
+if __name__ == "__main__":
+    print("=== Custom Garden Errors demo ===\n")
+
+    print("Testing PlantError...")
+    test_plant_error()
+
+    print("\nTesting WaterError...")
+    test_water_error()
+
+    print("\nTesting catching all garden errors...")
+    all_garden_errors_test()
+
+    print("\nAll custom error types work correctly")
