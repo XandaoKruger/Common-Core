9773f59ef7ab0ee084ab0207692edbb773df675a
acano-kr <acano-kr@student.42.fr>
2026-06-17 12:02:34 +0100

    Terminando o ex3 mod3

--- Milestone2/Python_Modules/Python_Module_03/ex3/ft_achievement_tracker.py
+++ Milestone2/Python_Modules/Python_Module_03/ex3/ft_achievement_tracker.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import random
 
+# CONQUISTAS - uppercase para indicar que é constante e global. 
+# Lista fixa, não muda durante a execução  
 CONQUISTAS = [
     'Crafting Genius', 'World Savior', 'Master Explorer', 'Collector Supreme', 
     'Untouchable', 'Boss Slayer', 'Strategist', 'Unstoppable', 'Speed Runner', 
@@ -10,6 +12,65 @@ CONQUISTAS = [
 
 
 def gen_player_achievements() -> set:
-    quantidade = random.randint(5,10)
+    # Randint para randomizar entre 5 e 9
+    quantidade = random.randint(5,9)
+
+    # Escolhe SEM repetir, dentro da quantidade (que é aleatorizada a cima)
     sorteio = random.sample(CONQUISTAS, quantidade)
     return set(sorteio)
+
+
+if __name__ == "__main__":
+    print("=== Achievement Track System ===\n")
+
+    # Variaveis de controle
+    alice = gen_player_achievements()
+    bob = gen_player_achievements()
+    charlie = gen_player_achievements()
+    dylan = gen_player_achievements()
+
+    # Lista que guarda tuplas, para utilizar no for abaixo. 
+    # ("Coloca o nome" para cada variavel)
+    exibicao= [
+        ("Alice", alice), ("Bob", bob), ("Charlie", charlie), ("Dylan", dylan)
+    ]
+
+    # For que para cada nome, usa a variavel na lista de exibicao
+    for nome, conjunto in exibicao:
+        print(f"Player {nome}: {conjunto}")
+
+    # Val para ver todas as conquistas que foram recebidas entre os jogadores
+    total_conqu = alice.union(bob).union(charlie).union(dylan)
+    print(f"\nAll distinct achievements: {total_conqu}")
+
+    # Var para ver a conquista que todos tem igual.
+    igual = alice.intersection(bob).intersection(charlie).intersection(dylan)
+    print(f"\nCommon achievements: {igual}\n")
+
+    for nome, conjunto in exibicao:
+        # Cria um saco vazio para guardar todos os achievments dos outros
+        outros = set()
+        # Mais um loop para comparar um jogador com outro jogador
+        for n, c in exibicao:
+            # Verificar se não é o mesmo jogador
+            if n != nome:
+                # Utiliza o saco vazio para receber e guardar as conquistas 
+                # do jogador, se for repetida, não coloca por causa do set.
+                # Faz isso para todos os jogadores, e no final compara com
+                # o jogador primário. 
+                outros = outros.union(c)
+
+        # Exclusivo recebe apenas a conquista que só o {nome} tem, difference
+        # se encarrega de observar se não há iguais, sobrando só as conquistas
+        # que aleque jogador tem em exclusivo.
+        # Resumo: Retirando o que os "outros" tem, do conjunto
+        exclusivo = conjunto.difference(outros)
+        print(f"Only {nome} has: {exclusivo}")
+
+    # Transformando as conquistas em set (conjunto) para poder executar as
+    # funções matematicas de set (union, difference, intersection...)
+    all_achiev= set(CONQUISTAS)
+    print()
+    for nome, conjunto in exibicao:
+        faltando = all_achiev.difference(conjunto)
+        print(f"{nome} is missing: {faltando}")
