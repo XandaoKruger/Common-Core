cb74ce77dd5913aa2e7101d2ce0d4c54c821b3a1
Alexandre Cano Kruger <acano-kr@c1r4s5.42lisboa.com>
2026-07-03 14:30:38 +0100

    ex 5

--- Milestone2/Python_Modules/Python_Module_03/ex5/ft_data_stream.py
+++ Milestone2/Python_Modules/Python_Module_03/ex5/ft_data_stream.py
@@ -11,4 +11,27 @@ ACTIONS = [
 
 
 def gen_event() -> typing.Generator[tuple[str, str], None, None]:
-    
+
+    rand_player = random.choice(PLAYERS)
+    rand_action = random.choice(ACTIONS)
+
+    # "Retorna" sem matar o processo a tupla com a ordem player -> action
+    yield (rand_player, rand_action)
+
+
+def consume_event()
+
+if __name__ == "__main__":
+
+    for i in range(1000):
+
+        # next para passar ao proximo yield a cada iteracao
+        gen = next(gen_event())
+        print(f"Event {i}: Player {gen[0]} did action {gen[1]}")
+
+    # Criando a lista de 10
+    lista_10: list[str] = []
+    for _ in range (10):
+        lista_10.append(gen)
+
+    print(f"\nBuilt list of 10 events: {lista_10}\n")
