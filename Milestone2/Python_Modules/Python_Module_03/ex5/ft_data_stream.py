75b8e390d43a6db1a26a85cc1438e6b1a2ac2570
acano-kr <acano-kr@student.42.fr>
2026-07-03 22:09:20 +0100

    ex5 mod3

--- Milestone2/Python_Modules/Python_Module_03/ex5/ft_data_stream.py
+++ Milestone2/Python_Modules/Python_Module_03/ex5/ft_data_stream.py
@@ -12,26 +12,44 @@ ACTIONS = [
 
 def gen_event() -> typing.Generator[tuple[str, str], None, None]:
 
-    rand_player = random.choice(PLAYERS)
-    rand_action = random.choice(ACTIONS)
+    while True:
+        rand_player = random.choice(PLAYERS)
+        rand_action = random.choice(ACTIONS)
 
-    # "Retorna" sem matar o processo a tupla com a ordem player -> action
-    yield (rand_player, rand_action)
+        # "Retorna" sem matar o processo a tupla com a ordem player -> action
+        yield (rand_player, rand_action)
 
 
-def consume_event()
+def consume_event(
+    events: list[tuple[str, str]]
+) -> typing.Generator[tuple[str, str], None, None]:
+    while events:
+        # Randomiza um número do tamanho da lista 
+        random_index = random.randint(0, len(events) - 1)
+
+        # Pop garante que antes de remover eu ainda tenha o dado para yield
+        remove = events.pop(random_index)
+        yield remove
 
 if __name__ == "__main__":
 
-    for i in range(1000):
+    # next para passar ao proximo yield a cada iteracao
+    gen = gen_event()
 
-        # next para passar ao proximo yield a cada iteracao
-        gen = next(gen_event())
-        print(f"Event {i}: Player {gen[0]} did action {gen[1]}")
+    for i in range(1000):
+        # Avança gen 1 yield: retoma o while suspenso e devolve a próxima tupla
+        event = next(gen)
+        print(f"Event {i}: Player {event[0]} did action {event[1]}")
 
     # Criando a lista de 10
-    lista_10: list[str] = []
+    lista_10: list[tuple[str, str]] = []
     for _ in range (10):
-        lista_10.append(gen)
+        lista_10.append(next(gen))
 
     print(f"\nBuilt list of 10 events: {lista_10}\n")
+
+    for evento in consume_event(lista_10):
+
+        print(f"Got event from list: {evento}")
+
+        print(f"Remains in list: {lista_10}\n")
