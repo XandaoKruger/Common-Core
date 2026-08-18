eeeeaa0bb58ca2717d5e46bd80a993bd0dc737fc
acano-kr <acano-kr@student.42.fr>
2026-08-18 11:11:15 +0100

    mod6 finish distillation

--- Milestone2/Python_Modules/Python_Module_06/alchemy/__init__.py
+++ Milestone2/Python_Modules/Python_Module_06/alchemy/__init__.py
@@ -1,4 +1,6 @@
 from .elements import create_air
+from .potions import healing_potion as heal
+from .potions import strength_potion
 
 # Forma de dizer que sei que não usei create_air aqui dentro (neste caso)
-__all__ = ["create_air"]
+__all__ = ["create_air", "heal", "strength_potion"]
