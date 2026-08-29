22b268fa3c81a59f7ca84d42f508aa77156e6502
acano-kr <acano-kr@student.42.fr>
2026-08-29 03:10:09 +0100

    mod7 ex0 finalizado com flk8 e mypy

--- Milestone2/Python_Modules/Python_Module_08/ex0/construct.py
+++ Milestone2/Python_Modules/Python_Module_08/ex0/construct.py
@@ -11,21 +11,48 @@ def in_virtual_env() -> bool:
 
 
 def report_inside() -> None:
-    '''Decisão de usar o sys.prefix, é o próprio interpretador reportando por si
-    mesmo, é fonte primária, já o os.enviren.get("VIRTUAL_ENV") pode puxar
+    '''Decisão de usar o sys.prefix, é o próprio interpretador reportando por
+    si mesmo, é fonte primária, já o os.enviren.get("VIRTUAL_ENV") pode puxar
     qualquer informação de algum venv antigo que não foi encerrado, um script
     que pode estar ausente ou desatualizado, é fonte secundária. No final
     fazem o mesmo, mas estruturalmente sys.prefix é mais confiável.'''
 
-    print(f"Current Python: {sys.executable}")
-    print(f"Environment Path: {sys.prefix}")
-    print(f"Virtual Environment: {os.path.basename(sys.prefix)}")
-    print(f"Package installation path:\n{site.getsitepackages()[0]}")
+    print("\n\033[32mMATRIX STATUS\033[m: Welcome to the construct\n")
+
+    print(f"\033[34mCurrent Python\033[m: {sys.executable}")
+    print(f"\033[34mVirtual Environment\033[m: {os.path.basename(sys.prefix)}")
+    print(f"\033[34mEnvironment Path\033[m: {sys.prefix}")
+
+    print(
+        "\n\033[32mSUCCESS\033[m: You're in an isolated enviroment!\
+\nSafe to install packages whithout affecting\
+\nthe global system.\n"
+        )
+
+    print(
+        f"\033[34mPackage installation path\033[m:\
+\n{site.getsitepackages()[0]}"
+        )
+
 
 def report_outside() -> None:
-    print(f"Current Python: {sys.executable}")
 
-os.path.basename
+    print("\n\033[32mMATRIX STATUS\033[m: You're still plugged in\n")
+
+    print(f"\033[34mCurrent Python\033[m: {sys.executable}")
+    print("\033[34mVirtual Environment\033[m: None detected")
+
+    print("\n\033[31mWARNING\033[m: You're in the global enviroment!\n\
+The machines can see everything you install.\n")
+
+    print("\033[36mTo enter the construct, run\033[m:")
+    print("\033[33mpython -m venv matrix_env\033[m")
+    print("source matrix_env/bin/activate # On Unix")
+    print("matrix_env\\scripts\\activate # On Windows")
+
+    print("\nThen run this program again.\n")
+
+
 def main() -> None:
     if in_virtual_env():
         report_inside()
